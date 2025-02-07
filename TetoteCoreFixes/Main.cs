eb2fbf807bf660e4e316411bbee1ba6a5d42cc88
kyoubate-haruka <46010460+kyoubate-haruka@users.noreply.github.com>
2025-02-07 15:36:33 +0100

    change description

--- TetoteCoreFixes/Main.cs
+++ TetoteCoreFixes/Main.cs
@@ -23,7 +23,7 @@ namespace TetoteCoreFixes {
             ConfigSkipAeroBootCheck = Config.Bind("General", "Skip Aero Boot Check", true, "Disables launching aeroBootCheckWnd.exe");
             ConfigPreventAutoScreenRotate = Config.Bind("General", "Prevent Screen Rotation", true, "Disables automatic screen rotation");
             ConfigCameraDummy = Config.Bind("General", "Camera Emulation", true, "Disable the camera requirement");
-            ConfigEndlessDates = Config.Bind("General", "Disable End Dates", true, "Disable end dates of various things such as songs or events");
+            ConfigEndlessDates = Config.Bind("General", "Disable End Dates", true, "Disable end dates of various things such as songs or the shop");
             ConfigSomePrices = Config.Bind("General", "Add prices to the shop", true, "Adds some random prices to the shop items. Better than having them all being zero I guess?");
             
             Harmony.CreateAndPatchAll(typeof(Patches), "eu.haruka.gmg.cf.teto");
